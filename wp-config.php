ff8ecf062fba3bd9f1eb1c5ad34e9a053b115949
cristhian2121 <ccdelgadop@gmail.com>
2020-12-10 00:28:52 -0500

    first commit

---
+++ wp-config.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define( 'DB_USER', 'wordpress');
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'wordpress');
+
+/** MySQL hostname */
+define( 'DB_HOST', 'db:3306');
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '10414d972d8af24db2852c804550ab1a59c3f6c9');
+define( 'SECURE_AUTH_KEY',  '255221bf4a308b9ad529a114ff304e30afda538d');
+define( 'LOGGED_IN_KEY',    '765e90d7a2d66c453ea3ab37ce573d4ab8194c12');
+define( 'NONCE_KEY',        'f66ae1920861a2ad1fdfb80f39ff462afb4dc203');
+define( 'AUTH_SALT',        '2f2ebe2ae5672beae4c522d0e569115e4fe2ff85');
+define( 'SECURE_AUTH_SALT', '2bda3c475fcc0af6c2f31dd9b1c99ce0f5bf46b1');
+define( 'LOGGED_IN_SALT',   '21bc19bf8ce324ac87f3efeb32957d85715b76f4');
+define( 'NONCE_SALT',       '1a83c9f06bde78e837a0c85c573e8a3aab2f3c13');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/* Our config */
+@ini_set( 'upload_max_filesize' , '128M' );
+@ini_set( 'post_max_size', '128M');
+@ini_set( 'memory_limit', '256M' );
+@ini_set( 'max_execution_time', '300' );
+@ini_set( 'max_input_time', '300' );
+
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
---
+++ wp-content/themes/hashone/template-parts/section-service.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ *
+ * @package HashOne
+ */
+$hashone_page = '';
+$hashone_page_array = get_pages();
+if (is_array($hashone_page_array)) {
+    $hashone_page = $hashone_page_array[0]->ID;
+}
+
+if (get_theme_mod('hashone_disable_service_sec') != 'on') {
+    ?>
+    <section id="hs-service-post-section" class="hs-section">
+        <div class="hs-service-left-bg"></div>
+        <div class="hs-container">
+            <div class="hs-service-posts">
+                <?php
+                $hashone_service_title = get_theme_mod('hashone_service_title', esc_html__('Why Choose Us', 'hashone'));
+                $hashone_service_sub_title = get_theme_mod('hashone_service_sub_title', esc_html__('Let us do all things for you', 'hashone'));
+                ?>
+
+                <?php if ($hashone_service_title) { ?>
+                    <!-- <h2 class="hs-section-title wow fadeInUp" data-wow-duration="0.5s"><?php echo esc_html($hashone_service_title); ?></h2> -->
+                <?php } ?>
+
+                <?php if ($hashone_service_sub_title) { ?>
+                    <!-- <div class="hs-section-tagline wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.5s"><?php echo esc_html($hashone_service_sub_title); ?></div> -->
+                <?php } ?>
+
+                <div class="hs-service-post-wrap">
+                    <?php
+                        
+                        // <!-- Todo: pass to variables KameHouse -->
+                        $services_actives = array(
+                            'Diseños de sistemas de seguridad y salud en el trabajo' => 
+                                "El diseño del sistema de gestión de la seguridad y salud en el trabajo está dirigido a empresas que por primera vez incorporarán este sistema en sus procesos misionales",
+                            'Implementación del sistema de seguridad y salud en el trabajo' => 
+                                "Para la implementación del sistema se tienen en cuenta la autoevaluación de las condiciones iniciales realizada bajo el diagnostico de la resolución 0312 de 2019 y las actividades del plan anual de trabajo, el cronograma de capacitación y formación",
+                            'Investigación de incidentes, accidentes y enfermedades laborales' => 
+                                "Se realiza la investigación de acuerdo con las directrices del ministerio de trabajo y las guías que instaura el ministerio de salud.",
+                            'Diseño del plan de emergencias' => 
+                                "el plan de emergencias se establece con el objetivo de preparar a las empresas frente a una eventual emergencia que puede ser causada por eventos de origen natural, tecnológico y social.",
+                            'Capacitación y entrenamiento a brigadas' => 
+                                "Con el propósito de que las personas actuén de manera adecuada y oportuna ante una emergencia, salvaguardando su propia vida y de las personas que estén rodeadas. proceso estipulado bajo la de la resolución 256 de 2014.",
+                            'Aplicación de batería de riesgo psicosocial' => 
+                                "Contamos con un equipo especializado en evaluar el riesgo intralaboral, extralaboral y el estrés al que se enfrenta el trabajador, a partir de los resulta dos se generan procedimientos para evitar patologías causadas por el estrés ocupacional.",
+                            'Servicios de auditorias' => 
+                                "La auditoría inicia con la definición y planificación del programa junto con el cronograma de interven ciones. una vez se realice la verificación del sg sst",
+                            'Diseño del PESV' => 
+                                "Te acompañamos en la creación del plan estratégico de seguridad vial",
+                            'Exámenes médicos ocupacionales' => 
+                                "Aplicación de exámenes de ingreso, periódicos o de egreso, además, se incluyen las pruebas complementarias como exámenes paraclinicos y pruebas de laboratorio"
+                                
+
+
+
+                        );
+                        // <!--  -->
+                    $i = 1;
+                    foreach ($services_actives as $key => $value) {
+
+                    // }
+
+                    // for ($i = 0; $i < count($services_actives); $i++) {
+                        $hashone_service_page_id = get_theme_mod('hashone_service_page' . $i, $hashone_page);
+                        $hashone_service_page_icon = get_theme_mod('hashone_service_page_icon' . $i, 'fa-globe');
+                        
+                        if ($hashone_service_page_id) {
+                            $args = array('page_id' => $hashone_service_page_id);
+                            $query = new WP_Query($args);
+                            if ($query->have_posts()):
+                                while ($query->have_posts()) : $query->the_post();
+                                    $hashone_wow_delay = ($i * 200) + 300;
+                                    ?>
+                                    <div class="hs-clearfix hs-service-post hs-service-post<?php echo $i; ?> wow fadeInDown" data-wow-duration="0.5s" data-wow-delay="<?php echo $hashone_wow_delay; ?>ms">
+                                        <div class="hs-service-icon">
+                                            <i class="fa <?php echo esc_attr($hashone_service_page_icon); ?>"></i>
+                                        </div>
+
+                                        <div class="hs-service-excerpt">
+                                            <h6><a href="<?php the_permalink(); ?>"><?php echo $key; //the_title(); ?></a></h6>
+                                            <?php
+                                                echo $value;
+                                            // if (has_excerpt()) {
+                                            //     echo get_the_excerpt();
+                                            // } else {
+                                            //     echo hashone_excerpt(get_the_content(), 100);
+                                            // }
+                                            ?>
+                                        </div>
+                                    </div>
+                                    <?php
+                                endwhile;
+                            endif;
+                            wp_reset_postdata();
+                        }
+                        $i++;
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php } ?>
